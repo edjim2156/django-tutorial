f509cb1b39a22b486faf65cef4db3ea803d0fba2
Edwin_Jimenez <ejimen10@charlotte.edu>
2025-02-22 01:29:43 -0500

    Static files

--- djangoProject/base/views.py
+++ djangoProject/base/views.py
@@ -100,8 +100,17 @@ def room(request, pk):
         room.participants.add(request.user)
         return redirect('room', pk = room.id)
 
-    contex ={'room': room, 'room_messages': room_messages, 'participants':participants }
-    return render(request, 'base/room.html', contex)
+    context ={'room': room, 'room_messages': room_messages, 'participants':participants }
+    return render(request, 'base/room.html', context)
+
+def userProfile(request, pk):
+    user = User.objects.get(id=pk)
+    rooms = user.room_set.all()
+    room_messages = user.message_set.all()
+    topics = Topic.objects.all()
+
+    context = {'user':user, 'rooms':rooms, 'room_messages':room_messages, 'topics':topics}
+    return render(request, 'base/profile.html', context)
 
 
 @login_required(login_url='login')
@@ -110,7 +119,9 @@ def createRoom(request):
     if request.method == 'POST':
         form = RoomForm(request.POST)
         if form.is_valid():
-            form.save()
+            room = form.save(commit=False)
+            room.host = request.user
+            room.save()
             return redirect('home')
 
     context = {'form' : form}
